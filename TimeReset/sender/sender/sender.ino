58c92b471857d3f67072ebb454d465647264d9fd
Adam S <azslomian@gmail.com>
2019-09-30 00:25:03 +0200

    sendeer in reset name refactor

--- TimeReset/sender/sender/sender.ino
+++ TimeReset/sender/sender/sender.ino
@@ -4,26 +4,20 @@
 
   unsigned long myMillis=0;   
   unsigned long prevMillis=0; 
-  int pin = 3;  
+  int resetPin = 3;  
   
   void setup() {
     Serial.begin( 9600 );
-    pinMode(pin, OUTPUT); 
+    pinMode(resetPin, OUTPUT); 
   }
 
   void loop() {
     myMillis = millis() - prevMillis;
     Serial.println(myMillis);
-    digitalWrite(pin, HIGH);
-    delay(100);
-    digitalWrite(pin, LOW);
-    delay(1000);
-   
-    
+    digitalWrite(resetPin, HIGH);
+    digitalWrite(resetPin, LOW);
     prevMillis = millis();
-    
+    delay(1000);
     delay(1000);            // waits for a second
     delay(1000);            // waits for a second
-    
-    delay(100); 
   }
