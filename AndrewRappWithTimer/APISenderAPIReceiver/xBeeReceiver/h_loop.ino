e41ba4b6efc4b5b1782a98ef42723e7c60e283ab
Adam <azslomian@gmail.com>
2019-10-03 05:59:45 -0700

    Time measurment changes

--- AndrewRappWithTimer/APISenderAPIReceiver/xBeeReceiver/h_loop.ino
+++ AndrewRappWithTimer/APISenderAPIReceiver/xBeeReceiver/h_loop.ino
@@ -7,9 +7,6 @@ void loop() {
 
 void getResponse(){
   if (xbee.getResponse().isAvailable()) {
-      // got something
-      
-      //Serial.println("got something");
       if (xbee.getResponse().getApiId() == ZB_RX_RESPONSE) {
         getPacket();
       } else if (xbee.getResponse().getApiId() == MODEM_STATUS_RESPONSE) {
@@ -30,16 +27,10 @@ void ControlAssociation(){
     Serial.println("we have got response");
     if (msr.getStatus() == ASSOCIATED) {
       Serial.println("yay this is great.");
-      // yay this is great.  flash led
-      flashLed(statusLed, 10, 10);
     } else if (msr.getStatus() == DISASSOCIATED) {
-      // this is awful.. flash led to show our discontent
       Serial.println("this is awful.. flash led to show our discontent");
-      flashLed(errorLed, 10, 10);
     } else {
-      // another status
       Serial.println("another status");
-      flashLed(statusLed, 5, 10);
     }
 }
 
@@ -50,29 +41,24 @@ void ControlResponseError(){
 
 void getPacket(){
   // got a zb rx packet
-  slaveTime = millis();
-  Serial.println("got a zb rx packet");
+  myClockMillis = millis() - prevClockMillis;
+  Serial.println(myClockMillis);
   // now fill our zb rx class
   xbee.getResponse().getZBRxResponse(rx);
-
-  if(isEndOfSync != 1){
-    //syncTimer(rx.getData());
-  }
-  
   ControlACK(); 
   // set dataLed PWM to value of the first byte in the data
-  Serial.println(dataLed, rx.getData(0));
-  analogWrite(dataLed, rx.getData(0));
+  //Serial.print(rx.getData(0));
+  //nalogWrite(dataLed, rx.getData(0));
 }
 
 void ControlACK(){
    if (rx.getOption() == ZB_PACKET_ACKNOWLEDGED) {
       // the sender got an ACK
-      Serial.println("the sender got an ACK");
-      flashLed(statusLed, 10, 10);
+      //Serial.println("the sender got an ACK");
+      //flashLed(statusLed, 10, 10);
   } else {
       // we got it (obviously) but sender didn't get an ACK
-      Serial.println("we got it (obviously) but sender didn't get an ACK");
-      flashLed(errorLed, 2, 20);
+      //Serial.println("we got it (obviously) but sender didn't get an ACK");
+      //flashLed(errorLed, 2, 20);
   }
 }
