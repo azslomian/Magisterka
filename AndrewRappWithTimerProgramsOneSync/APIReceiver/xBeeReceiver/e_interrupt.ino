a822e4428e70d0cec9b7e3ef5de168012b91bd9d
Adam <azslomian@gmail.com>
2019-10-09 07:53:40 -0700

    changes in receiver

--- AndrewRappWithTimerProgramsOneSync/APIReceiver/xBeeReceiver/e_interrupt.ino
+++ AndrewRappWithTimerProgramsOneSync/APIReceiver/xBeeReceiver/e_interrupt.ino
@@ -1,6 +1,4 @@
 void resetClock()           
 {
-     //myClockMillis = millis() - prevClockMillis;                                             
-     //Serial.println(myClockMillis);
      prevClockMillis = millis();
 }
