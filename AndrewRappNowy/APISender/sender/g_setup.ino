cd9ff35f6bcabdfd254f83daa7f4a1a50a867a27
Adam S <azslomian@gmail.com>
2019-10-23 14:19:09 +0200

    API sender changed

--- AndrewRappNowy/APISender/sender/g_setup.ino
+++ AndrewRappNowy/APISender/sender/g_setup.ino
@@ -2,7 +2,7 @@ void setup() {
   pinMode(statusLed, OUTPUT);
   pinMode(errorLed, OUTPUT);
   pinMode(resetPin, OUTPUT);
-  Serial1.begin(38400);
+  Serial1.begin(115200);
   xbee.setSerial(Serial1);
   Serial.begin(9600);
   
--- AndrewRappWithTimerProgramsOneSync/APISender/sender/h_loop.ino
+++ AndrewRappWithTimerProgramsOneSync/APISender/sender/h_loop.ino
@@ -12,7 +12,7 @@ void loop() {
     inByte = Serial.read();
     switch (inByte) {
     case 'l':    
-      for(int i = 0; i < 1; i++){
+      for(int i = 0; i < 20; i++){
         myMillis = millis() - prevMillis;  
         xbee.send(zbTxU);
         ControlResp();
